3928935a69c581a665f643ab632ea2ee16374e6e
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2022-05-08 00:24:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,16 @@
 
 const express = require("express");
 const history = require("connect-history-api-fallback");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const service = require('./service.js');
 
 const app = express();
 
 app.use("/", express.static("dist"));  // file server for the frontend
 app.use(history());  // fallback
-app.use(bodyParser.json())  // JSON parser
+app.use(bodyParser.json());  // JSON parser
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', service)  // REST APIs
+app.use('/api', service);  // REST APIs
 
 const host = process.argv[2] || "localhost";
 const port = process.argv[3] || 8080;
